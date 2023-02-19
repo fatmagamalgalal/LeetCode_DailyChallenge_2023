a34ae98cb6d3d3fe010244bfb00cdcd99ecc9fd5
Fatma Gamal <43317809+fatmagamalgalal@users.noreply.github.com>
2023-02-19 19:54:26 +0200

    Create 16- Maximum Depth of Binary Tree (Fatma Gamal).py

---
+++ 02- February/16- Maximum Depth of Binary Tree/16- Maximum Depth of Binary Tree (Fatma Gamal).py
@@ -0,0 +1,17 @@
+// Author: Fatma Gamal Eldin Galal
+  
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+
+        l = self.maxDepth(root.left)
+        r = self.maxDepth(root.right)
+        # reurn max depth of the subtrees
+        return max(l,r) + 1
